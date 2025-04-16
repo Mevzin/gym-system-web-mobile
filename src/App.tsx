3d080c997f4d933c0cb651ddd76e99bddd3bff4b
Thiago Torres <thiagomev@gmail.com>
2025-04-16 08:33:21 -0300

    force pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import './App.css'
 import Home from './pages/home'
-
+import { useEffect, useState } from 'react'
 function App() {
+  const [deferredPrompt, setDeferredPrompt] = useState<any>(null)
+  const [showInstallBtn, setShowInstallBtn] = useState(false)
 
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault()
+      setDeferredPrompt(e)
+      setShowInstallBtn(true)
+    })
+  }, [])
+
+  const handleInstallClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt()
+      deferredPrompt.userChoice.then(() => {
+        setDeferredPrompt(null)
+        setShowInstallBtn(false)
+      })
+    }
+  }
   return (
     <>
       <Home />
+      {showInstallBtn && (
+        <button onClick={handleInstallClick}>
+          Instalar app
+        </button>
+      )}
     </>
   )
 }
