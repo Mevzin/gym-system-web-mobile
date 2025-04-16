d00203cc0ce66743a0d3437e377329c12bc65487
Thiago Torres <thiagomev@gmail.com>
2025-04-16 04:21:47 -0300

    fix: Change inputs on signup

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -2,12 +2,12 @@ const SignUp = () => {
     return (
         <div className="flex flex-col gap-2 w-[300px] items-center">
             <h1 className="text-slate-200 text-2xl font-bold ">CADASTRO</h1>
-            <input placeholder="Usuario" className="h-10 placeholder:pl-3  w-[300px]" />
-            <input placeholder="Senha" type="password" className="h-10 placeholder:pl-3 w-[300px]" />
-            <input placeholder="Email" type="password" className="h-10 placeholder:pl-3 w-[300px]" />
-            <input placeholder="Nome" className="h-10 placeholder:pl-3  w-[300px]" />
-            <input placeholder="Idade" type="password" className="h-10 placeholder:pl-3 w-[300px]" />
-            <input placeholder="Peso" className="h-10 placeholder:pl-3  w-[300px]" />
+            <input placeholder="Usuario" className="h-10   w-[300px] pl-3" />
+            <input placeholder="Senha" type="password" className="h-10  w-[300px] pl-3 " />
+            <input placeholder="Email" type="email" className="h-10  w-[300px] pl-3" />
+            <input placeholder="Nome" className="h-10   w-[300px] pl-3" />
+            <input placeholder="Idade" type="text" className="h-10  w-[300px] pl-3" />
+            <input placeholder="Peso" className="h-10   w-[300px] pl-3" />
 
             <button className="bg-orange-500 w-[250px] h-10 mt-3 font-bold text-xl"> Cadastrar</button>
             <p className="mt-3">Esqueceu sua senha?</p>
