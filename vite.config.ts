6deac14a731bf3c0343da7fd9aac68f633cf1a41
Thiago Torres de Souza <thiagomev@gmail.com>
2025-07-07 13:31:28 -0300

    fix: manifest changes

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,11 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon"
+          },
+          {
             src: 'icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
