4d17d46b4c297c53ce5fb4bd75d1893aad199cd3
Thiago Torres de Souza <thiagomev@gmail.com>
2025-07-07 07:06:49 -0300

    fix: Change call to api

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,11 @@ import axios from "axios"
 export const apiBase = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 })
+
+export const apiUser = axios.create({
+    baseURL: `${import.meta.env.VITE_API_URL}/user`
+})
+
+export const apiFile = axios.create({
+    baseURL: `${import.meta.env.VITE_API_URL}/user`
+})
